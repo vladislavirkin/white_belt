d8b578459ba7c087436a74459cf8d7c81124ed1a
vladislavirkin <vladislavirkin@outlook.com>
2021-03-13 11:20:10 +0300

    week 1 task 5 cost of goods

--- whiteBeltApp/whiteBeltApp/main.cpp
+++ whiteBeltApp/whiteBeltApp/main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a, b;
-    cin >> a >> b;
+int main() 
+{
+    double n, a, x, b, y;
 
-    if (b == 0) {
-        cout << "Impossible" << endl;
-    } else {
-        cout << a / b << endl;
+    cin >> n >> a >> b >> x >> y;
+
+    if (n > b)
+    {
+        cout << n * (1 - y / 100);
+    } 
+    else if (n > a)
+    {        
+        cout << n * (1 - x / 100);
+    }
+    else
+    {
+        cout << n;
     }
 
     return 0;
