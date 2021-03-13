7ef16a6ecf535aeac0bc6d5a1a900603eaa4a0d8
vladislavirkin <vladislavirkin@outlook.com>
2021-03-13 14:28:03 +0300

    week 2 task 4 UpdateIfGreater

--- whiteBeltApp/whiteBeltApp/main.cpp
+++ whiteBeltApp/whiteBeltApp/main.cpp
@@ -1,55 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
 using namespace std;
 
-bool IsPalindrom(const string str)
+void UpdateIfGreater(int first, int& second)
 {
-    int size = str.size();
-    for (int i = 0; i < size / 2 + 1; i++)
-    {
-        if (str[i] != str[size - 1 - i])
-            return false;
-    }
-
-    return true;
-}
-
-vector<string> PalindromFilter(const vector<string> words, const int minLength)
-{
-    vector<string> result;
-    for (auto str : words)
-    {
-        if (str.size() >= minLength)
-        {
-            if (IsPalindrom(str))
-                result.push_back(str);
-        }
-    }
-
-    return result;
+    if (first > second)
+        second = first;
 }
 
 int main() 
 {
-    vector<string> words;
+    int a, b;
 
-    string str = " ";
-    int minLength = 0;
+    cin >> a >> b;
 
-    cin >> minLength;
+    UpdateIfGreater(a, b);
 
-    while (str != "exit")
-    {
-        cin >> str;
-        words.push_back(str);
-    }    
-
-    words = PalindromFilter(words, minLength);
-
-    for (int i = 0; i < words.size(); ++i)
-        cout << words[i] << ", ";
+    cout << b;
 
     return 0;
 }
