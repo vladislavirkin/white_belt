e657f5a6a891e150323da9d47c995cf56ca06cb6
vladislavirkin <vladislavirkin@outlook.com>
2021-03-16 20:30:37 +0300

    week 3 task 7 class person 2 // another solution

--- whiteBeltApp/whiteBeltApp/main.cpp
+++ whiteBeltApp/whiteBeltApp/main.cpp
@@ -13,6 +13,76 @@
 
 using namespace std;
 
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> FindNamesHistory(const map<int, string>& names_by_year,
+    int year) {
+    vector<string> names;
+    // перебираем всю историю в хронологическом пор€дке
+    for (const auto& item : names_by_year) {
+        // если очередное им€ не относитс€ к будущему и отличаетс€ от предыдущего,
+        if (item.first <= year && (names.empty() || names.back() != item.second)) {
+            // добавл€ем его в историю
+            names.push_back(item.second);
+        }
+    }
+    return names;
+}
+
+string BuildJoinedName(vector<string> names) {
+    // нет истории Ч им€ неизвестно
+    if (names.empty()) {
+        return "";
+    }
+    // мен€ем пр€мой хронологический пор€док на обратный
+    reverse(begin(names), end(names));
+
+    // начинаем строить полное им€ с самого последнего
+    string joined_name = names[0];
+
+    // перебираем все последующие имена
+    for (int i = 1; i < names.size(); ++i) {
+        if (i == 1) {
+            // если это первое Ђисторическоеї им€, отдел€ем его от последнего скобкой
+            joined_name += " (";
+        }
+        else {
+            // если это не первое им€, отдел€ем от предыдущего зап€той
+            joined_name += ", ";
+        }
+        // и добавл€ем очередное им€
+        joined_name += names[i];
+    }
+
+    // если в истории было больше одного имени, мы открывали скобку Ч закроем еЄ
+    if (names.size() > 1) {
+        joined_name += ")";
+    }
+    // им€ со всей историей готово
+    return joined_name;
+}
+
+// см. решение предыдущей задачи
+string BuildFullName(const string& first_name, const string& last_name) {
+    if (first_name.empty() && last_name.empty()) {
+        return "Incognito";
+    }
+    else if (first_name.empty()) {
+        return last_name + " with unknown first name";
+    }
+    else if (last_name.empty()) {
+        return first_name + " with unknown last name";
+    }
+    else {
+        return first_name + " " + last_name;
+    }
+}
+
 class Person {
 public:
     void ChangeFirstName(int year, const string& first_name) {
@@ -21,98 +91,46 @@ public:
     void ChangeLastName(int year, const string& last_name) {
         last_names[year] = last_name;
     }
+
     string GetFullName(int year) {
-        string first_name = getLastChanged(year, first_names);
-        string last_name = getLastChanged(year, last_names);
-        if (first_name.empty() && last_name.empty())
-            return "Incognito";
-        else {
-            if (first_name.empty())
-                return last_name + " with unknown first name";
-            else {
-                if (last_name.empty())
-                    return first_name + " with unknown last name";
-            }
-        }
+        // найдЄм историю изменений имени и фамилии
+        // в данном случае это излишне, так как нам достаточно узнать последние им€ и фамилию, но почему бы не использовать готовые функции?
+        const vector<string> first_names_history = FindFirstNamesHistory(year);
+        const vector<string> last_names_history = FindLastNamesHistory(year);
 
-        return first_name + " " + last_name;
-    }
-    string GetFullNameWithHistory(int year){
-        string first_name = getLastChangedWithHistory(year, first_names);
-        string last_name = getLastChangedWithHistory(year, last_names);
-        if (first_name.empty() && last_name.empty())
-            return "Incognito";
-        else {
-            if (first_name.empty())
-                return last_name + " with unknown first name";
-            else {
-                if (last_name.empty())
-                    return first_name + " with unknown last name";
-            }
+        // подготовим данные дл€ функции BuildFullName: возьмЄм последние им€ и фамилию или оставим их пустыми, если они неизвестны
+        string first_name;
+        string last_name;
+        if (!first_names_history.empty()) {
+            first_name = first_names_history.back();
         }
-
-        return first_name + " " + last_name;
+        if (!last_names_history.empty()) {
+            last_name = last_names_history.back();
+        }
+        return BuildFullName(first_name, last_name);
     }
+
+    string GetFullNameWithHistory(int year) {
+        // получим полное им€ со всей историей
+        const string first_name = BuildJoinedName(FindFirstNamesHistory(year));
+        // получим полную фамилию со всей историей
+        const string last_name = BuildJoinedName(FindLastNamesHistory(year));
+        // объединим их с помощью готовой функции
+        return BuildFullName(first_name, last_name);
+    }
+
 private:
+    vector<string> FindFirstNamesHistory(int year) {
+        return FindNamesHistory(first_names, year);
+    }
+    vector<string> FindLastNamesHistory(int year) {
+        return FindNamesHistory(last_names, year);
+    }
+
     map<int, string> first_names;
     map<int, string> last_names;
-
-    string getLastChanged(const int& year, map<int, string>& m) {
-        string result;
-        for (const auto& item : m) {
-            if (item.first <= year)
-                result = item.second;
-            else
-                break;
-        }
-
-        return result;
-    }
-    string getLastChangedWithHistory(const int& year, map<int, string>& m) {
-        vector<string> history;        
-        for (const auto& item : m) {
-            if (item.first <= year)
-                history.push_back(item.second);
-            else
-                break;
-        }
-        reverse(begin(history), end(history));  
-        history = buildUniqueHistory(history);
-
-        string result;
-
-        if (history.size() > 0) {
-            result = history[0];
-            if (history.size() > 1) {                
-                    result += " (" + history[1];
-                if (history.size() > 2) {
-                    for (int i = 2; i < history.size(); ++i) {                        
-                            result += ", " + history[i];
-                    }
-                }
-                result += ")";
-            }
-        }                  
-
-        return result;
-    }    
-
-    vector<string> buildUniqueHistory(vector<string>& v) {
-        vector<string> result;
-
-        if (v.size() == 0)
-            return v;
-
-        result.push_back(v[0]);
-
-        for (auto& item : v) {
-            if (item != result.back())
-                result.push_back(item);
-        }
-
-        return result;
-    }
 };
+
 
 int main() {
     Person person;
