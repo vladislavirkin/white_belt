8d916273f54e272338c6ee46bee5fd28ebe4d3e8
vladislavirkin <vladislavirkin@outlook.com>
2021-03-17 13:42:17 +0300

    week 4 task 1 right vs wrong initialization

--- whiteBeltApp/whiteBeltApp/main.cpp
+++ whiteBeltApp/whiteBeltApp/main.cpp
@@ -1,20 +1,79 @@
-//Определите тип Incognizable, для которого следующий код будет корректен :
+//Допишите конструктор и структуры Specialization, Course, Week так, 
+//чтобы объект LectureTitle можно было бы корректно создатьс помошью right initialization,
+//и нельзя было создать с помощью wrong initialization
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-struct Incognizable {
-    int x = 0;
-    int y = 0;
+struct Specialization {
+    string value;
+
+    explicit Specialization(const string& new_value) {
+        value = new_value;
+    }
+};
+
+struct Week {
+    string value;
+
+    explicit Week(const string& new_value) {
+        value = new_value;
+    }
+};
+
+struct Course {
+    string value;
+
+    explicit Course(const string& new_value) {
+        value = new_value;
+    }
+};
+
+struct LectureTitle {
+    string specialization;
+    string course;
+    string week;
+
+    LectureTitle(const Specialization& new_specialization, const Course& new_course,
+        const Week& new_week) {
+        specialization = new_specialization.value;
+        course = new_course.value;
+        week = new_week.value;
+    }
 };
 
 int main() {    
-    Incognizable a;
-    Incognizable b = {};
-    Incognizable c = { 0 };
-    Incognizable d = { 0, 1 };
+    // right initialization
+    
+    LectureTitle title(
+        Specialization("C++"),
+        Course("White belt"),
+        Week("4th")
+    );
+
+    // wrong initialization
+
+    /*LectureTitle title("C++", "White belt", "4th");
+
+    LectureTitle title(string("C++"), string("White belt"), string("4th"));
+
+    LectureTitle title = { "C++", "White belt", "4th" };
+
+    LectureTitle title = { {"C++"}, {"White belt"}, {"4th"} };
+
+    LectureTitle title(
+        Course("White belt"),
+        Specialization("C++"),
+        Week("4th")
+    );
+
+    LectureTitle title(
+        Specialization("C++"),
+        Week("4th"),
+        Course("White belt")
+    );*/
 
     return 0;    
 }
