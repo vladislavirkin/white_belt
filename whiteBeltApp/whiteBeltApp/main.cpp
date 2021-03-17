67d8735240141c5c1bd601ec4cd2d908bdd0fbb3
vladislavirkin <vladislavirkin@outlook.com>
2021-03-18 00:47:51 +0300

    week 4 task 5 formatting output

--- whiteBeltApp/whiteBeltApp/main.cpp
+++ whiteBeltApp/whiteBeltApp/main.cpp
@@ -1,42 +1,24 @@
-//В этом задании вам предстоит написать две небольшие программы.Каждый пункт - отдельная задача, 
-//решение отправляйте в поле с соответствующим номером.
-//
-//Часть 1
-//
-//Ваша программа должна считать содержимое файла input.txt и напечатать его на экран без изменений.
-//Гарантируется, что содержимое файла input.txt заканчивается переводом строки.
-//
-//Часть 2
-//
-//Снова считайте все содержимое файла input.txt, но на этот раз выведите его в файл output.txt.
-//Точно так же гарантируется, что содержимое файла input.txt заканчивается переводом строки.
+//В файле input.txt записаны вещественные числа, по одному на строчку.
+//На стандартный вывод напечатайте эти числа в том же порядке, по одному на строке, 
+//но сделайте так, чтобы у каждого из них было ровно три знака после десятичной точки.
 
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    string line;
-    // part 1
+    double number;    
 
     ifstream input("input.txt");    
     if (input) {
-        while (getline(input, line)) {
-            cout << line << endl;
+        cout << fixed << setprecision(3);
+        while (input >> number) {
+            cout << number << endl;
         }
     }
-
-    //part 2
-
-    /*ifstream input("input.txt");
-    ofstream output("output.txt");
-    if (input) {
-        while (getline(input, line)) {
-            output << line << endl;
-        }
-    }*/
     
     return 0;
 }
