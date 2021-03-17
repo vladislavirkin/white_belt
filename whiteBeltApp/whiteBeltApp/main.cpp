b1fe2dfb40042101c1c1e788042c282e3834da6f
vladislavirkin <vladislavirkin@outlook.com>
2021-03-18 00:31:09 +0300

    week 4 task 4 input/output

--- whiteBeltApp/whiteBeltApp/main.cpp
+++ whiteBeltApp/whiteBeltApp/main.cpp
@@ -1,128 +1,42 @@
-//Реализуйте рассказанный на лекции класс Function, позволяющий создавать, 
-//вычислять и инвертировать функцию, состоящую из следующих элементарных операций :
+//В этом задании вам предстоит написать две небольшие программы.Каждый пункт - отдельная задача, 
+//решение отправляйте в поле с соответствующим номером.
 //
-//прибавить вещественное число x;
-//вычесть вещественное число x.
+//Часть 1
 //
-//При этом необходимо объявить константными все методы, которые по сути такими являются.
+//Ваша программа должна считать содержимое файла input.txt и напечатать его на экран без изменений.
+//Гарантируется, что содержимое файла input.txt заканчивается переводом строки.
 //
-//Замечание
+//Часть 2
 //
-//Более детальное описание задачи с подробным разбором реализации вышеуказанного 
-//класса приводится в двух предшествующих видеолекциях.
-//На проверку пришлите файл, содержащий реализацию вышеуказанного класса Function.
+//Снова считайте все содержимое файла input.txt, но на этот раз выведите его в файл output.txt.
+//Точно так же гарантируется, что содержимое файла input.txt заканчивается переводом строки.
 
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-struct Image {
-    double quality;
-    double freshness;
-    double rating;
-};
-
-struct Params {
-    double a;
-    double b;
-    double c;
-};
-
-class FunctionPart {
-public:
-    FunctionPart(char new_operation, double new_value) {
-        operation = new_operation;
-        value = new_value;
-    }
-    double Apply(double source_value) const {
-        if (operation == '+') {
-            return source_value + value;
-        }
-        if (operation == '-') {
-            return source_value - value;
-        }
-        if (operation == '*') {
-            return source_value * value;
-        }
-        if (operation == '/') {
-            return source_value / value;
-        }        
-        return 0;
-    }
-    void Invert() {
-        if (operation == '+') {
-            operation = '-';
-        }
-        else {
-            if (operation == '-') {
-                operation = '+';
-            }
-            else {
-                if (operation == '*') {
-                    operation = '/';
-                }
-                else {
-                    if (operation == '/') {
-                        operation = '*';
-                    }
-                }
-            }
-        }                     
-    }
-private:
-    char operation;
-    double value;
-};
-
-class Function {
-public:
-    void AddPart(char operation, double value) {
-        parts.push_back({ operation, value });
-    }
-    double Apply(double value) const {
-        for (const FunctionPart& part : parts) {
-            value = part.Apply(value);
-        }
-        return value;
-    }
-    void Invert() {
-        for (FunctionPart& part : parts) {
-            part.Invert();
-        }
-        reverse(begin(parts), end(parts));
-    }
-private:
-    vector<FunctionPart> parts;
-};
-
-Function MakeWeightFunction(const Params& params,
-    const Image& image) {
-    Function function;
-    function.AddPart('*', params.a);
-    function.AddPart('-', image.freshness * params.b);
-    function.AddPart('+', image.rating * params.c);
-    return function;
-}
-
-double ComputeImageWeight(const Params& params, const Image& image) {
-    Function function = MakeWeightFunction(params, image);
-    return function.Apply(image.quality);
-}
-
-double ComputeQualityByWeight(const Params& params,
-    const Image& image,
-    double weight) {
-    Function function = MakeWeightFunction(params, image);
-    function.Invert();
-    return function.Apply(weight);
-}
-
 int main() {
-    Image image = { 10, 2, 6 };
-    Params params = { 4, 2, 6 };
-    cout << ComputeImageWeight(params, image) << endl;
-    cout << ComputeQualityByWeight(params, image, 52) << endl;
+    string line;
+    // part 1
+
+    ifstream input("input.txt");    
+    if (input) {
+        while (getline(input, line)) {
+            cout << line << endl;
+        }
+    }
+
+    //part 2
+
+    /*ifstream input("input.txt");
+    ofstream output("output.txt");
+    if (input) {
+        while (getline(input, line)) {
+            output << line << endl;
+        }
+    }*/
+    
     return 0;
 }
