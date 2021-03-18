0be7654db1eb8a8457221f82b9571e9445af25ff
vladislavirkin <vladislavirkin@outlook.com>
2021-03-18 17:58:59 +0300

    week 4 task 10 Rational exceptions

--- whiteBeltApp/whiteBeltApp/main.cpp
+++ whiteBeltApp/whiteBeltApp/main.cpp
@@ -1,21 +1,168 @@
 #include <iostream>
+#include <numeric>
+#include <fstream>
 #include <string>
+#include <sstream>
+#include <iomanip>
+#include <set>
+#include <vector>
+#include <map>
 #include <exception>
 using namespace std;
 
-void EnsureEqual(const string& left, const string& right) {
-    if (left != right) {
-        throw runtime_error(left + " != " + right);
+class Rational {
+public:
+    Rational() {
+        p = 0;
+        q = 1;
+        reduceFraction();
     }
+
+    Rational(int numerator, int denominator) {
+        p = numerator;
+        q = denominator;
+        validateFraction();
+        reduceFraction();
+    }
+
+    int Numerator() const {
+        return p;
+    }
+
+    int Denominator() const {
+        return q;
+    }
+
+    void Set(int numerator, int denominator) {
+        p = numerator;
+        q = denominator;
+        validateFraction();
+        reduceFraction();
+    }
+
+    void SetNumerator(const int& numerator) {
+        p = numerator;
+        validateFraction();
+        reduceFraction();
+    }
+
+    void SetDenominator(const int& denominator) {
+        q = denominator;
+        validateFraction();
+        reduceFraction();
+    }
+
+private:
+    int p, q;
+
+    void validateFraction() {
+        if (q == 0) {
+            throw invalid_argument("q = 0");
+        }
+
+        if (p * q > 0) {
+            p = abs(p);
+            q = abs(q);
+        }
+        if (q < 0 && p > 0) {
+            p = -1 * p;
+            q = abs(q);
+        }
+        if (p == 0) {
+            q = 1;
+        }
+    }
+
+    void reduceFraction() {
+        int GCD = 0;
+        while (GCD != 1) {
+            GCD = gcd(p, q);
+            p /= GCD;
+            q /= GCD;
+        }
+    }
+};
+
+Rational operator+(const Rational& lhs, const Rational& rhs) {
+    int numerator = lhs.Numerator() * rhs.Denominator() + rhs.Numerator() * lhs.Denominator();
+    int denominator = lhs.Denominator() * rhs.Denominator();
+    return Rational(numerator, denominator);
+}
+
+Rational operator-(const Rational& lhs, const Rational& rhs) {
+    int numerator = lhs.Numerator() * rhs.Denominator() - rhs.Numerator() * lhs.Denominator();
+    int denominator = lhs.Denominator() * rhs.Denominator();
+    return Rational(numerator, denominator);
+}
+
+Rational operator/(const Rational& lhs, const Rational& rhs) {
+    if (rhs.Numerator() == 0) {
+        throw domain_error("/0");
+    }
+    int numerator = lhs.Numerator() * rhs.Denominator();
+    int denominator = rhs.Numerator() * lhs.Denominator();
+    return Rational(numerator, denominator);
+}
+
+Rational operator*(const Rational& lhs, const Rational& rhs) {
+    int numerator = lhs.Numerator() * rhs.Numerator();
+    int denominator = lhs.Denominator() * rhs.Denominator();
+    return Rational(numerator, denominator);
+}
+
+bool operator==(const Rational& lhs, const Rational& rhs) {
+    Rational result = lhs - rhs;
+    return (result.Numerator() == 0);
+}
+
+bool operator<(const Rational& lhs, const Rational& rhs) {
+    Rational result = lhs - rhs;
+    return (result.Numerator() < 0);
+}
+
+bool is_stream_empty(istream& pFile)
+{
+    return pFile.peek() == istream::traits_type::eof();
+}
+
+istream& operator>>(istream& stream, Rational& rational) {   
+    int numerator, denominator;    
+
+    stream >> numerator;
+    if (stream.peek() != '/')
+        return stream;
+    stream.ignore(1);
+    if (stream.peek() == -1)
+        return stream;
+    stream >> denominator;
+    if (denominator > 0)   
+        rational.Set(numerator, denominator);        
+
+    return stream;  
+}
+
+ostream& operator<<(ostream& stream, const Rational& rational) {
+    stream << rational.Numerator() << "/" << rational.Denominator();
+    return stream;
 }
 
 int main() {
     try {
-        EnsureEqual("C++ White", "C++ White");
-        EnsureEqual("C++ White", "C++ Yellow");
+        Rational r(1, 0);
+        cout << "Doesn't throw in case of zero denominator" << endl;
+        return 1;
     }
-    catch (runtime_error& e) {
-        cout << e.what() << endl;
+    catch (invalid_argument&) {
     }
+
+    try {
+        auto x = Rational(1, 2) / Rational(0, 1);
+        cout << "Doesn't throw in case of division by zero" << endl;
+        return 2;
+    }
+    catch (domain_error&) {
+    }
+    cout << "OK" << endl;
+
     return 0;
 }
